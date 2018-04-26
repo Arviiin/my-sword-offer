6f0006351988f26b12c5b2317b5269152729883c
Arviiin <931639826@qq.com>
2018-04-26 12:43:27 +0800

    long commit1234

---
+++ src/cn/edu/dhu/swordoffer/Algorithm07Fibonacci.java
@@ -0,0 +1,27 @@
+package cn.edu.dhu.swordoffer;
+
+/**
+ * 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项。
+ * n<=39
+ */
+public class Algorithm07Fibonacci {
+    public static int Fibonacci(int n) {
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        int a = 1;
+        int b = 1;
+        int temp = 0;
+        while(n > 2){
+            temp = a+b;
+            a = b;
+            b = temp;
+            n -- ;
+        }
+        return temp;
+    }
+    public static void main(String [] args){
+        int n =5;
+        System.out.print(Fibonacci(n));
+    }
+}
---
+++ src/cn/edu/dhu/swordoffer/Algorithm08JumpFloor.java
@@ -0,0 +1,22 @@
+package cn.edu.dhu.swordoffer;
+
+/**
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ * 思路，从最后登上最高台阶考虑，比如10，那么只有两种可能，从第8个台阶跨两步或者从第9个台阶跨1步。
+ *       然后分别把第8个台阶，第9个台阶视为最高台阶如前所述向前递推，容易知道f（10） = f（9）+f（8），也即是
+ *         f（n） = f（n-1）+ f（n-2）
+ */
+public class Algorithm08JumpFloor {
+    public static int JumpFloor(int target) {
+        if (target == 1)
+            return 1;
+        if (target == 2)
+            return 2;
+        return JumpFloor(target-1) + JumpFloor(target-2);
+
+    }
+    public static void main(String [] args){
+        int n =5;
+        System.out.print(JumpFloor(n));
+    }
+}
