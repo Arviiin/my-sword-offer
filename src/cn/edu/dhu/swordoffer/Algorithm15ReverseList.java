4535d52dd03a8fcff744974911c11e7e4d88b7b4
Arviiin <931639826@qq.com>
2018-05-07 09:38:54 +0800

    long commit 15

---
+++ src/cn/edu/dhu/swordoffer/Algorithm15ReverseList.java
@@ -0,0 +1,37 @@
+package cn.edu.dhu.swordoffer;
+
+import cn.edu.dhu.datastruct.ListNode;
+
+import static cn.edu.dhu.datastruct.ListNode.GenNodeList;
+
+/**
+ * 输入一个链表，反转链表后，输出链表的所有元素。
+ */
+//思路：利用指针p ,q ，head，用p来在断掉链表之前保存后面的数据
+public class Algorithm15ReverseList {
+    public static ListNode ReverseList(ListNode head) {
+        if(head == null){
+            return null;
+        }else if(head.next == null){
+            return head;
+        }else {
+            ListNode p = null;
+            ListNode q = null;
+            p = head.next;
+            head.next= q;//将原本头结点的next域置为空
+            while(p != null){
+                q = p;
+                p = p.next;
+                q.next = head;
+                head = q;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode nodelist = GenNodeList();
+        ListNode listNode = ReverseList(nodelist);
+        System.out.println(listNode.val);
+    }
+}
